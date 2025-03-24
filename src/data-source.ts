c2c64b5143c26efbd008dc5d719b7b958627b3ec
Karthisri <ksri80734@gmail.com>
2025-03-24 09:41:36 +0530

    updated commit

---
+++ src/data-source.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+import { DataSource, Migration } from "typeorm";
+import { User } from "./entities/User"; 
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "localhost", 
+  port: 3306, 
+  username: "root",
+  password: "123Test@",
+  database: "test1",
+  synchronize: false, 
+  logging: true,
+  entities: [User],
+  migrations: [`${process.cwd()}/src/migration/*.ts`],  
+//  subscribers: [], 
+});
+
+// Establishing connection with the database
+export const connectDB = async () => {
+  try {
+    await AppDataSource.initialize(); // Initialize the DataSource
+    console.log("Data Source has been initialized!");
+  } catch (error) {
+    console.error("Error during Data Source initialization:", error);
+  }
+};
